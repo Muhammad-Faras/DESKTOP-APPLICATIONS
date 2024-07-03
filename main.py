24624d9ee48251b956f7f654589651fee395f79b
Muhammad Faras <noorfaras809@gmail.com>
2024-07-03 17:45:26 +0500

    organizing code in classes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QWidget,QMainWindow, QPushButton
 
 import sys
+
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('first desktop app')
+        button = QPushButton('Click Me')
+        self.setCentralWidget(button)
+        
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.setWindowTitle('first pyside6 app')
-button = QPushButton()
-button.setText('clcik me')
-window.setCentralWidget(button)
+window = ButtonHolder()
 window.show()
 app.exec()
