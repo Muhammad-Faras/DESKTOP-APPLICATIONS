c666d44e0aedfc6326c2a0ab10fb2cbbfb659cc3
Muhammad Faras <noorfaras809@gmail.com>
2024-07-04 09:01:32 +0500

    added menubar

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from PySide6.QtWidgets import QApplication
 from button_holder import ButtonHolder
 from slider import SliderHolder
 from QWidget import RockWidget
+from menubar import MenuClass
 import sys  
 
 app = QApplication(sys.argv)
-window = RockWidget()
+window = MenuClass(app)
 window.show()
 app.exec()
---
+++ menubar.py
@@ -0,0 +1,29 @@
+from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Slot
+
+class MenuClass(QMainWindow):
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+        # Create a menu bar
+        menu_bar = self.menuBar()
+
+        # Create a file menu
+        file_menu = menu_bar.addMenu("&File")
+        quit_app = file_menu.addAction('Quit')
+        quit_app.triggered.connect(self.exit_application_slot)
+        
+        
+        # Create a file menu
+        edit_menu = menu_bar.addMenu("&Edit")
+        edit_menu_action = edit_menu.addAction('Copy')
+        edit_menu_action = edit_menu.addAction('Cut')
+        edit_menu_action = edit_menu.addAction('Paste')
+        edit_menu_action = edit_menu.addAction('Undo')
+        edit_menu_action = edit_menu.addAction('Redo')
+        
+    @Slot()
+    def exit_application_slot(self):
+        print('app exit')
+        self.app.exit()
+        
